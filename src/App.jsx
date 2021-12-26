023f953c55a6ce57ecf600f5bd057b66099e7159
Uriel Aispuro <urielalexis64@gmail.com>
2021-12-25 21:07:57 -0700

    add localStorage persistance

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
 	const [pacientes, setPacientes] = useState([]);
+	const [paciente, setPaciente] = useState({});
+
+	useEffect(() => {
+		const listado = localStorage.getItem("pacientes");
+		if (listado) {
+			setPacientes(JSON.parse(listado));
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("pacientes", JSON.stringify(pacientes));
+	}, [pacientes]);
+
+	const deletePaciente = (id) => {
+		const filteredPacientes = pacientes.filter((paciente) => paciente.id !== id);
+		setPacientes(filteredPacientes);
+	};
 
 	return (
 		<div className='container mx-auto mt-20'>
 			<Header />
 			<div className='mt-12 md:flex '>
-				<Formulario setPacientes={setPacientes} />
-				<ListadoPacientes pacientes={pacientes} />
+				<Formulario
+					pacientes={pacientes}
+					setPacientes={setPacientes}
+					paciente={paciente}
+					setPaciente={setPaciente}
+				/>
+				<ListadoPacientes
+					pacientes={pacientes}
+					setPaciente={setPaciente}
+					deletePaciente={deletePaciente}
+				/>
 			</div>
 		</div>
 	);
